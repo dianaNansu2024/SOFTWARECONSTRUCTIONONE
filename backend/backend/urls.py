c9512ed4f6dc336c548b823722a2c6a9eddaaed1
dianaNansu2024 <promise24556@gmail.com>
2025-02-25 11:41:53 +0300

    added end point connection for urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users.views import UserListCreate
+from tasks.views import TaskListCreate
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('users/', UserListCreate.as_view(), name='users-list'),
+    path('tasks/', TaskListCreate.as_view(), name='tasks-list'),
 ]
+

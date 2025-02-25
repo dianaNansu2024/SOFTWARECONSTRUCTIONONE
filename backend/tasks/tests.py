648f6e26be20f795137af4790d152fa67031f7b3
dianaNansu2024 <promise24556@gmail.com>
2025-02-25 22:31:28 +0300

    added Unit tests for users and task API

--- backend/tasks/tests.py
+++ backend/tasks/tests.py
@@ -1,3 +1,13 @@
+
 from django.test import TestCase
+from .models import Task
+
+class TaskTests(TestCase):
+    def setUp(self):
+        self.task = Task.objects.create(title="Test Task")
+
+    def test_task_list(self):
+        response = self.client.get("/tasks/")
+        self.assertEqual(response.status_code, 200)
 
 # Create your tests here.
--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+class UserTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="password")
+
+    def test_user_list(self):
+        response = self.client.get("/users/")
+        self.assertEqual(response.status_code, 200)
+
 
 # Create your tests here.

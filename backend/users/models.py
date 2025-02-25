b70bde5133fca9a0f4956109966fa690d01ad3d8
dianaNansu2024 <promise24556@gmail.com>
2025-02-25 11:36:48 +0300

    done migrations

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,16 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
 
-# Create your models here.
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="customuser_set",  # Avoids conflict with the default user model
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="customuser_set",  # Avoids conflict with the default user model
+        blank=True
+    )
